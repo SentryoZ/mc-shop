77c041918adc7909ffa3c5e4539f66e3938565a9
tAmh <ttrhuynh07@gmail.com>
2024-11-11 01:42:23 -0800

    updated

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.head')
+
+@section('title', 'Register')
+
+@section('content')
+    <div class="container-xl vh-100 align-content-center">
+        <div class="row align-self-center border rounded-3 p-4">
+            <div class="col-md-8 mx-auto align-self-center">
+                <img src="https://static.vecteezy.com/system/resources/previews/003/689/228/non_2x/online-registration-or-sign-up-login-for-account-on-smartphone-app-user-interface-with-secure-password-mobile-application-for-ui-web-banner-access-cartoon-people-illustration-vector.jpg"
+                    class="img-fluid" alt="Responsive image">
+
+            </div>
+
+            <div class="col-md-4 d-flex align-items-center justify-content-center">
+                <div class="w-100" style="max-width: 400px;">
+                    <h2 class="text-center mb-4">Create an Account</h2>
+                    <form action="/register" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                                <input type="text" class="form-control" id="name" placeholder="Enter your full name"
+                                    required>
+                                @error('name')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="fa-solid fa-envelope"></i></span>
+                                <input type="email" class="form-control" id="email" placeholder="Enter your email"
+                                    required>
+                                @error('email')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                                <input type="password" class="form-control" id="password" placeholder="Create a password"
+                                    required>
+                                @error('password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="confirm-password" class="form-label">Confirm Password</label>
+                            <div class="input-group">
+                                <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                                <input type="password" class="form-control" id="confirm-password"
+                                    placeholder="Confirm your password" required>
+                                @error('confirm-password')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                    <p class="text-center mt-3">
+                        Already have an account? <a href="/login" class="text-decoration-none">Log in</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+Route::post('/register', [RegisterController::class, 'getData']);
+
+Route::get('/login', [LoginController::class, 'index']);
