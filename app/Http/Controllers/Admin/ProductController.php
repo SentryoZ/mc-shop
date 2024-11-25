807758b12d1d0a8ee9f80374f2cd98b8b6ee3214
tAmh <ttrhuynh07@gmail.com>
2024-11-24 22:24:36 -0800

    .

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Models\ProductImages;
 use App\Models\ProductCommands;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
